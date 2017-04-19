814db044ecef4aaaa7bd05f68b97f0574b2bd200
Peter Jenkins <pjenkins@mparticle.com>
2017-04-19 15:53:29 -0400

    Fix readme

--- mparticle-example/platforms/ios/MParticleExample/Plugins/cordova-plugin-mparticle/CDVMParticle.m
+++ mparticle-example/platforms/ios/MParticleExample/Plugins/cordova-plugin-mparticle/CDVMParticle.m
@@ -264,7 +264,7 @@ typedef NS_ENUM(NSUInteger, MPCDVCommerceEventAction) {
     product.sku = json[@"sku"];
     product.variant = json[@"variant"];
     product.position = [json[@"position"] intValue];
-    product.quantity = json[@"quantity"];
+    product.quantity = json[@"quantity"] ?: @1;
     NSDictionary *jsonAttributes = json[@"customAttributes"];
     for (NSString *key in jsonAttributes) {
         NSString *value = jsonAttributes[key];
